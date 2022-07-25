597c9412b916ae19d6161a72d3ad4914f48aa499
JiRad71 <kirilllkorolev@gmail.com>
2022-07-25 20:31:46 +0300

    feat: fix problem

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 /* ************************************************************************************************
  *                                                                                                *
  * Please read the following tutorial before implementing tasks:                                   *
@@ -7,7 +6,7 @@
  *                                                                                                *
  ************************************************************************************************ */
 
-const { json } = require("stream/consumers");
+// const { json } = require("stream/consumers");
 
 
 /**
